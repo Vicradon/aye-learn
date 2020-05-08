684604828a0e809b7216235ad4772b2bba2833c3
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-05-08 17:05:47 +0100

    Add create and read to categories

--- app/routes/categories.js
+++ app/routes/categories.js
@@ -3,6 +3,21 @@ const categoryController = require("../controllers/category");
 const router = Router();
 const hasAccessTo = require('../controllers/utils/hasAccessTo')
 
+router.post('/new',
+  hasAccessTo('createAny', 'category'),
+  categoryController.createCategory
+)
+
+router.get('/:id',
+  hasAccessTo('readAny', 'category'),
+  categoryController.getCategory
+)
+
+router.get('/',
+  hasAccessTo('readAny', 'category'),
+  categoryController.getAllCategories
+)
+
 router.patch('/:id',
   hasAccessTo('updateAny', 'category'),
   categoryController.updateCategory
